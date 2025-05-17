0816fe79717a5eda9a46233367066774b4a5ca63
wangmaosong <wangmsd@dcits.com>
2025-05-17 14:44:06 +0800

    feat：微服务整合nacos

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # fly
 根据新版本和一些新的思考做一些新的测试和实现 。
+
+
+
+# 参考文档
+* https://github.com/alibaba/spring-cloud-alibaba/blob/2023.x/README-zh.md
+* https://java2ai.com/docs/1.0.0-M6.1/concepts/?spm=4347728f.33449ac1.0.0.6f68556b27wBu1
+* https://github.com/nacos-group/nacos-docker/blob/master/example/standalone-mysql.yaml
+* https://sca.aliyun.com/docs/2023/overview/version-explain/?spm=5238cd80.47ee59c.0.0.189fcd36D6yMOA
--- fly-account/sky-account-service/src/main/java/com/wxf/fly/account/AccountApplication.java
+++ fly-account/sky-account-service/src/main/java/com/wxf/fly/account/AccountApplication.java
@@ -2,6 +2,7 @@ package com.wxf.fly.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Account Service
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Wxf
  * @since 2025-05-08 17:18:06
  **/
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AccountApplication {
 
